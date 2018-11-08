42e264309d22f5f02265e05c2774b1c1c9490921
Dalton Rooney <dalton@madebyraygun.com>
2018-11-07 16:42:27 -0800

    Add support for background fill color

--- src/variables/SvgplaceholderVariable.php
+++ src/variables/SvgplaceholderVariable.php
@@ -28,8 +28,8 @@ use Craft;
  */
 class SvgplaceholderVariable
 {
-    public function generate($dimensions) {
-		$src = '<svg width="'.$dimensions['width'].'" height="'.$dimensions['height'].'" viewBox="0 0 '.$dimensions['width'].' '.$dimensions['height'].'" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h108v100H0z" fill="none"/></svg>';
-    return 'data:image/svg+xml;base64,'.base64_encode($src); 
+    public function generate($dimensions,$fill="none") {
+		$src = '<svg width="'.$dimensions['width'].'" height="'.$dimensions['height'].'" viewBox="0 0 '.$dimensions['width'].' '.$dimensions['height'].'" xmlns="http://www.w3.org/2000/svg"><rect x="0" y="0" width="'.$dimensions['width'].'" height="'.$dimensions['height'].'" fill="'.$fill.'" /></svg>';
+    return 'data:image/svg+xml;base64,'.base64_encode($src);
 	}
 }
